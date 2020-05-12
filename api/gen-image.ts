2458cfaf088629331f24a7174d64bc42855e0637
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-05-11 23:10:39 -0300

    feat: Add cache

--- api/gen-image.ts
+++ api/gen-image.ts
@@ -1,7 +1,12 @@
 import { NowRequest, NowResponse } from '@now/node';
 
+const CACHE_SECONDS = 60 * 60 * 24 * 30 * 5;
+
 export default (req: NowRequest, res: NowResponse) => {
+  res.setHeader('Cache-Control', `s-maxage=${CACHE_SECONDS}`);
+
   return res.json({
+    date: new Date().toLocaleString('en-US'),
     __data__: req.query
   });
 };
