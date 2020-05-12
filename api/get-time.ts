ff81f303301d21d7a3acc01a497bf61250ba2f6a
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-05-11 22:22:02 -0300

    feat: Test cache

--- api/get-time.ts
+++ api/get-time.ts
@@ -1,7 +1,13 @@
 import { NowRequest, NowResponse } from '@now/node';
 
+const cache = new Map<number, boolean>();
+
 export default (req: NowRequest, res: NowResponse) => {
+  const now = Date.now();
+  cache.set(now, true);
+
   res.json({
-    serverTime: Date.now()
+    serverTime: Date.now(),
+    __cache__: [...cache.entries()]
   });
 };
