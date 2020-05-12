aa8beaa7217bb5d03e4f32acfed440bc9455c177
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2020-05-11 22:32:38 -0300

    feat: Add some kind of authorization

--- api/get-time.ts
+++ api/get-time.ts
@@ -2,7 +2,20 @@ import { NowRequest, NowResponse } from '@now/node';
 
 const cache = new Map<number, boolean>();
 
+if (!process.env.GET_TIME_SECRET) {
+  throw new Error('Missing `GET_TIME_SECRET` environment variable.');
+}
+
 export default (req: NowRequest, res: NowResponse) => {
+  if (req.query.secret !== process.env.GET_TIME_SECRET) {
+    return res.status(403).json({
+      error: {
+        message: 'Invalid authorization token.',
+        code: 'ERR_INVALID_AUTHORIZATION_TOKEN'
+      }
+    });
+  }
+
   const now = Date.now();
   cache.set(now, true);
 
